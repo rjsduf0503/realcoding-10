38dafcc9840696969507b391c87009e2453da891
Byun sumi <selina2000@naver.com>
2021-04-05 21:42:38 +0900

    setup

---
+++ src/main/java/org/cnu/realcoding/Realcoding10Application.java
@@ -0,0 +1,13 @@
+package org.cnu.realcoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Realcoding10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Realcoding10Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/cnu/realcoding/Realcoding10ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cnu.realcoding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Realcoding10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
