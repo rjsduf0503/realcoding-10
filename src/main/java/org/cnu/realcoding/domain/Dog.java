ca57105363971744e583594785cbe87dfb7de90a
Byun sumi <selina2000@naver.com>
2021-04-05 22:50:24 +0900

    create Dog class

---
+++ src/main/java/org/cnu/realcoding/domain/Dog.java
@@ -0,0 +1,11 @@
+package org.cnu.realcoding.domain;
+
+import java.util.List;
+
+public class Dog {
+    private String name;
+    private String kind;
+    private String ownerName;
+    private String ownerPhoneNumber;
+    private List<String> medicalRecords;
+}
