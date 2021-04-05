af3b7742cf2d4ff641fc3d6f94243c15113bebf0
ryu <rjsduf0503@naver.com>
2021-04-06 01:42:09 +0900

    update findDogByThreeParam(name,ownerName,ownerPhoneNumber)

--- src/main/java/org/cnu/realcoding/repository/DogRepository.java
+++ src/main/java/org/cnu/realcoding/repository/DogRepository.java
@@ -3,7 +3,11 @@ package org.cnu.realcoding.repository;
 import org.cnu.realcoding.domain.Dog;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public class DogRepository {
@@ -12,5 +16,17 @@ public class DogRepository {
 
     public void insertDog(Dog dog){
         mongoTemplate.insert(dog);
+    }
+
+
+    public List<Dog> findDogsByNameAndOwnerNameAndOwnerPhoneNumber(String name, String ownerName, String ownerPhoneNumber){
+        List<Dog> dogs = mongoTemplate.find(
+                Query.query(
+                        Criteria.where("name").is(name).and
+                                ("ownerName").is(ownerName).and
+                                ("ownerPhoneNumber").is(ownerPhoneNumber)
+                ), Dog.class
+        );
+        return dogs;
     }
 }
--- src/main/java/org/cnu/realcoding/service/DogManagementService.java
+++ src/main/java/org/cnu/realcoding/service/DogManagementService.java
@@ -1,9 +1,12 @@
 package org.cnu.realcoding.service;
 
 import org.cnu.realcoding.domain.Dog;
+import org.cnu.realcoding.exception.DogsNotFoundException;
 import org.cnu.realcoding.repository.DogRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class DogManagementService {
@@ -14,4 +17,13 @@ public class DogManagementService {
     public void insertDog(Dog dog) {
         dogRepository.insertDog(dog);
     }
+
+    public List<Dog> getDogsByNameAndOwnerNameAndOwnerPhoneNumber(String name, String ownerName, String ownerPhoneNumber){
+        List<Dog> dogs = dogRepository.findDogsByNameAndOwnerNameAndOwnerPhoneNumber(name, ownerName, ownerPhoneNumber);
+        if(dogs.isEmpty()) {
+            throw new DogsNotFoundException();
+        }
+        return dogs;
+    }
+
 }
